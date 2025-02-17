c4432784c42a595281a52fdb1c4a5a4387970ae5
Rishi Ravikumar <rishiravi.k98@gmail.com>
2025-02-17 17:04:48 -0500

    fix: update generate.py
    Signed-off-by: Rishi Ravikumar <rishiravi.k98@gmail.com>

--- generate_script/generate.py
+++ generate_script/generate.py
@@ -3,6 +3,7 @@ import requests
 import json
 
 from datasets import load_dataset
+
 ds = load_dataset("Maxscha/commitbench")
 
 OLLAMA_URL = "http://localhost:11434/api/generate"
@@ -10,24 +11,33 @@ SEED = 42
 
 
 def call_ollama_model(model: str, prompt: str) -> str:
-    payload = {"model": model, "prompt": prompt, "stream": False, "options": {"temperature": 0.5, "seed": SEED}}
+    payload = {
+        "model": model,
+        "prompt": prompt,
+        "format": {"type": "object", "properties": {"message": {"type": "string"}}},
+        "stream": False,
+        "options": {"temperature": 0.5, "seed": SEED},
+    }
     response = requests.post(OLLAMA_URL, data=json.dumps(payload))
 
     # Parse and display the response
     try:
         if response.status_code == 200:
             result = response.json()
-            parsed_response = result["response"].split("\n\n")[1]
+            parsed_response = json.loads(result["response"])
 
-            return parsed_response.strip("\"")
+            return parsed_response["message"]
     except:
         return f"err: {response.text}"
+
 
 def main():
     options, _ = getopt.getopt(sys.argv[1:], "m:", "model=")
     model_name = ""
     if len(options) == 0:
-        print(f"missing --m option \n\nExample Usage: `python generate.py --m=llama3.2:1b`")
+        print(
+            f"missing --m option \n\nExample Usage: `python generate.py --m=llama3.2:1b`"
+        )
         return
     else:
         model_name = options[0][1]
@@ -39,7 +49,6 @@ def main():
             prompt = f"""The following is a diff which describes the code changes in a commit, Your task is to write a short commit message accordingly. {diff} According to the diff, the commit message should be:"""
             generated_commit_message = call_ollama_model(model_name, prompt)
             op.write(generated_commit_message + "\n")
-
 
 
 if __name__ == "__main__":
