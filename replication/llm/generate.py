bff86ac8b8f93c97c920fffd376a43e3e014c804
Thanh Phan <ph_thanh@live.concordia.ca>
2025-02-21 20:53:35 -0500

    fix: update LLM replication script

--- replication/llm/generate.py
+++ replication/llm/generate.py
@@ -1,33 +1,46 @@
 import getopt, sys
-import asyncio
-import json
+import requests
 import os
 import time
 import json
 
-from ollama import AsyncClient
-
 OLLAMA_URL = "http://localhost:11434/api/generate"
 SEED = 42
-client = AsyncClient()
 
 
-async def call_ollama_model(model: str, prompt: str) -> str:
-    response = await client.chat(
-        model=model,
-        messages=[{"role": "user", "content": prompt}],
-        format={"type": "object", "properties": {"message": {"type": "string"}}},
-        options={"temperature": 0.5, "seed": SEED},
-    )
-    parsed_response = json.loads(response.message.content)
-    print(parsed_response)
-    return parsed_response["message"]
+def call_ollama_model(model: str, prompt: str) -> str:
+    payload = {
+        "model": model,
+        "prompt": prompt,
+        "format": {"type": "object", "properties": {"message": {"type": "string"}}},
+        "stream": False,
+        "options": {"temperature": 0.5, "seed": SEED},
+    }
+    response = requests.post(OLLAMA_URL, data=json.dumps(payload))
+
+    # Parse and display the response
+    try:
+        if response.status_code == 200:
+            result = response.json()
+            parsed_response = json.loads(result["response"])
+            print(parsed_response)
+            return parsed_response["message"]
+    except:
+        return f"err: {response.text}"
 
 
-async def main():
+def main():
     start_time = time.time()
-    options, _ = getopt.getopt(sys.argv[1:], "ml:", ["model="])
+    options, _ = getopt.getopt(sys.argv[1:], "m:", "model=")
     model_name = ""
+
+    if len(options) == 0:
+        print(
+            f"missing --m option \n\nExample Usage: `python generate.py --m=llama3.2:1b`"
+        )
+        return
+    else:
+        model_name = options[0][1]
 
     with open("all_result.json", "r") as f:
         ds = json.load(f)
@@ -39,19 +52,17 @@ async def main():
     filename = f"output/{model_name.replace(':', '_')}.msg"
     os.makedirs(os.path.dirname(filename), exist_ok=True)
 
-    tasks = []
-    row_count = 0
-    for _, data in enumerate(ds):
-        row_count += 1
-        diff = data["diff"]
-        prompt = f"""The following is a diff which describes the code changes in a commit, Your task is to write a short commit message accordingly. {diff} According to the diff, the commit message should be:"""
-        tasks.append(call_ollama_model(model_name, prompt))
-
-    results = await asyncio.gather(*tasks)
     with open(filename, "w", encoding="utf-8") as op:
-        for _, commit_msg in enumerate(results):
-            op.write(commit_msg + "\n")
-    print(f"processed {row_count} row(s) for {model_name} in {time.time() - start_time} seconds")
+        # global row_count
+        for _, data in enumerate(ds):
+            diff = data["diff"]
+
+            prompt = f"""The following is a diff which describes the code changes in a commit, Your task is to write a short commit message accordingly. {diff} According to the diff, the commit message should be:"""
+            generated_commit_message = call_ollama_model(model_name, prompt)
+            op.write(generated_commit_message + "\n")
+
+        # print(f"processed {row_count} row(s) for {model_name} in {time.time() - start_time} seconds")
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
