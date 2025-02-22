df6c4e4def42a422d36b05858367087a8aa54e66
Thanh Phan <ph_thanh@live.concordia.ca>
2025-02-21 23:13:48 -0500

    fix: add proper logging support

--- replication/llm/generate.py
+++ replication/llm/generate.py
@@ -16,17 +16,23 @@ def call_ollama_model(model: str, prompt: str) -> str:
         "stream": False,
         "options": {"temperature": 0.5, "seed": SEED},
     }
-    response = requests.post(OLLAMA_URL, data=json.dumps(payload))
 
     # Parse and display the response
     try:
-        if response.status_code == 200:
-            result = response.json()
-            parsed_response = json.loads(result["response"])
-            print(parsed_response)
-            return parsed_response["message"]
-    except:
-        return f"err: {response.text}"
+        response = requests.post(OLLAMA_URL, data=json.dumps(payload))
+
+        try:
+            if response.status_code == 200:
+                result = response.json()
+                parsed_response = json.loads(result["response"])
+                return True, response.text, parsed_response["message"]
+            else:
+                return False, response.text, None
+        except Exception as e1:
+            return False, str(e1), None
+
+    except Exception as e2:
+        return False, str(e2), None
 
 
 def main():
@@ -49,19 +55,33 @@ def main():
         if opt in ("-m", "--model"):
             model_name = arg
 
-    filename = f"output/{model_name.replace(':', '_')}.msg"
+    sluggified_model_name = model_name.replace(":", "_").replace("/", "_")
+    filename = f"output/{sluggified_model_name}.msg"
+    filename_log = f"output/{sluggified_model_name}.csv"
     os.makedirs(os.path.dirname(filename), exist_ok=True)
+    os.makedirs(os.path.dirname(filename_log), exist_ok=True)
 
-    with open(filename, "w", encoding="utf-8") as op:
-        # global row_count
-        for _, data in enumerate(ds):
+    with open(filename, "w", encoding="utf-8") as op, open(filename_log, "w", encoding="utf-8") as log:
+        n = len(ds)
+        row_count = 0
+        print(f"""Processing {n} diffs via the model {model_name}""")
+        for i in range(n):
+            row_count += 1
+            data = ds[i]
             diff = data["diff"]
 
             prompt = f"""The following is a diff which describes the code changes in a commit, Your task is to write a short commit message accordingly. {diff} According to the diff, the commit message should be:"""
-            generated_commit_message = call_ollama_model(model_name, prompt)
-            op.write(generated_commit_message + "\n")
+            is_success, original_response, generated_commit_message = call_ollama_model(model_name, prompt)
 
-        # print(f"processed {row_count} row(s) for {model_name} in {time.time() - start_time} seconds")
+            if is_success:
+                print(f"{i}: {generated_commit_message}")
+                op.write(f"{i}: {generated_commit_message}\n")
+            else:
+                print(f"Failed to generate commit message for {i}th diff")
+
+            log.write(f"{i},\"{diff}\",\"{original_response}\"\n")
+
+        print(f"processed {row_count} row(s) for {model_name} in {time.time() - start_time} seconds")
 
 
 if __name__ == "__main__":
