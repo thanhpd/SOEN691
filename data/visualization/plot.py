97f25f43c408a2e760771f6ac33334f05f6cf379
Thanh Phan <ph_thanh@live.concordia.ca>
2025-04-17 12:07:10 -0400

    docs: add statistic distribution in RQ1

--- data/visualization/plot.py
+++ data/visualization/plot.py
@@ -82,10 +82,28 @@ def plot_rougel_boxplot(csv_file, lang):
     # Show the plot
     plt.show()
 
+def plot_meteor_boxplot(csv_file, lang):
+    df = pd.read_csv(csv_file)
+    df['METEOR'] = pd.to_numeric(df['METEOR'], errors='coerce')
+
+    # Create the boxplot
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(x='ModelName', y='METEOR', data=df)
+    plt.xticks(rotation=45)
+    plt.title(f"Box Plot of METEOR by Model ({lang})")
+    plt.xlabel("Model Name")
+    plt.ylabel("METEOR")
+    plt.grid(True, linestyle='--', alpha=0.6)
+
+    # Show the plot
+    plt.show()
+
 plot_bmoses_boxplot("./replication-lines/output_lines_java.csv", "java")
-plot_bnorm_boxplot("./replication-lines/output_lines_java.csv", "java")
-plot_bnltk_boxplot("./replication-lines/output_lines_java.csv", "java")
-plot_rougel_boxplot("./replication-lines/output_lines_java.csv", "java")
+# plot_bnorm_boxplot("./replication-lines/output_lines_java.csv", "java")
+# plot_bnltk_boxplot("./replication-lines/output_lines_java.csv", "java")
+# plot_rougel_boxplot("./replication-lines/output_lines_java.csv", "java")
+# plot_meteor_boxplot("./replication-lines/output_lines_java.csv", "java")
+# plot_bert_score_boxplot("./replication-lines-bert/output_lines_java.csv", "java")
 
 # plot_bert_score_boxplot("/content/data/output_bert_per_line_java.csv", "java")
 # plot_bert_score_boxplot("/content/data/output_bert_per_line_js.csv", "js")
